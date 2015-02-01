620e3dbde180b939f8fee9ef71f063c6ac1aac29
Petteri Kivimäki <dinky_jackson@hotmail.com>
2015-02-01 15:07:20 +0200

    New ClientUtil class added.
    Added a new ClientUtil class that provides helper methods for using
    HTTPS when connecting to a service. The methods are ment to be used in
    development, not in production.

---
+++ src/client/src/main/java/com/pkrete/xrd4j/client/util/ClientUtil.java
@@ -0,0 +1,127 @@
+/**
+ * MIT License
+ *
+ * Copyright (C) 2014 Petteri Kivimäki
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.pkrete.xrd4j.client.util;
+
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class offers some helper methods for SOAP client.
+ *
+ * @author Petteri Kivimäki
+ */
+public class ClientUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientUtil.class);
+
+    /**
+     * Calling this method creates a trust manager that does not validate
+     * certificate chains like the default. In practice, to access a HTTPS URL
+     * without having the certificate in the truststore.
+     *
+     * This method is for testing purposes only, and it must not be used in
+     * production. This method should only be used in scenarios, where it is
+     * not possible to install the required certificates using keytool e.g.
+     * local testing with temporary certificates.
+     *
+     * @throws Exception if there's an error
+     */
+    static public void doTrustToCertificates() throws Exception {
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+                }
+            }
+        };
+
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        HostnameVerifier hv = new HostnameVerifier() {
+
+            @Override
+            public boolean verify(String urlHostName, SSLSession session) {
+                if (!urlHostName.equalsIgnoreCase(session.getPeerHost())) {
+                    logger.warn("Warning: URL host \"{}\" is different to SSLSession host \"{}\".", urlHostName, session.getPeerHost());
+                    return false;
+                }
+                return true;
+            }
+        };
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+        // Now you it's possible to acces a https URL without having the
+        // certificate in the truststore
+    }
+
+    /**
+     * This methods overrides the default host name verifier that can cause
+     * problems with self signed or untrusted SSL certificates. The problem
+     * occurs when the subject alternative name (CN) in the certificate doesn't
+     * match the hostname of service URL. This custom verifier verifies that
+     * the host name is an acceptable match with the server's authentication
+     * scheme. However, the certificate must be added to the truststore to
+     * make this method work.
+     *
+     * This approach poses some security risks. Therefore, be mindful of that
+     * especially if you consider this solution for a production deployment.
+     */
+    public static void setCustomHostNameVerifier() {
+        HostnameVerifier hv = new HostnameVerifier() {
+
+            @Override
+            public boolean verify(String urlHostName, SSLSession session) {
+                if (!urlHostName.equalsIgnoreCase(session.getPeerHost())) {
+                    logger.warn("Warning: URL host \"{}\" is different to SSLSession host \"{}\".", urlHostName, session.getPeerHost());
+                    return false;
+                }
+                return true;
+            }
+        };
+        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    }
+}
